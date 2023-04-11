89bf3fc063d76f699953d9d71865be7040724cdc
Celina Kortmann <celina.kortmann@tu-dortmund.de>
2023-04-11 15:19:47 +0200

    Tabelle

--- tab.py
+++ tab.py
@@ -12,6 +12,6 @@ from uncertainties.unumpy import uarray                     # Array von Fehler: 
 from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
                                   std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
 
-md = pd.read_csv('HelmholtzspuleAus3.csv')
+md = pd.read_csv('Abstand3cm.csv')
 # md_a = md.iloc[:, [0,1]]
-print(md.to_latex(index = False, column_format= "c c", decimal=',')) 
+print(md.to_latex(index = False, column_format= "c c c c c", decimal=',')) 
